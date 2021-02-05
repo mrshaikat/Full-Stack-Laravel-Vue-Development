f5d060cfd1d14ccfcae90be21605662cd3ded675
Ashikujjaman Shaikat <72566703+mrshaikat@users.noreply.github.com>
2021-02-05 21:29:31 +0600

    Vue Admin Panel Loaded Done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 require('./bootstrap');
+window.Vue = require('vue')
+import router from './router'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+Vue.use(ViewUI);
 
- window.Vue = require('vue')
- import router from './router'
+Vue.component('mainapp', require('./components/mainapp.vue').default);
+const app = new Vue({
 
-    Vue.component('mainapp', require('./components/mainapp.vue').default);
-    const app = new Vue({
-
-        el:'#app',
-        router
-    })
+    el: '#app',
+    router
+})
--- resources/js/router.js
+++ resources/js/router.js
@@ -7,29 +7,55 @@ import newRoure from './components/pages/newFirstVuePage.vue'
 import hooks from './components/pages/basic/hooks.vue'
 import methods from './components/pages/basic/methods.vue'
 
+
+//Project Pages
+import home from './components/pages/home'
+import tags from './components/pages/tags'
 const routes = [
 
     //projects route...
 
     {
-        path: '/my-new-vue-route',
-        component: firstPage
+        path: '/',
+        component: home,
+    },
+
+    {
+        path: '/tags',
+        component: tags,
     },
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    //basic tutorial ...
+    {
+        path: '/my-new-vue-route',
+        component: firstPage,
+    },
     {
         path: '/new-route',
-        component: newRoure
+        component: newRoure,
     },
     {
         path: '/hooks',
-        component: hooks
+        component: hooks,
     },
 
     {
         path: '/methods',
-        component: methods
+        component: methods,
     },
 ]
 
